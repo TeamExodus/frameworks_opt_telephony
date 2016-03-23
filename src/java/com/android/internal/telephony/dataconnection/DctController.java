7e2a64bd6acf83bfe545895f4b0256f271c9a3f1
Susheel nyamala <snyamala@codeaurora.org>
2016-03-23 23:44:11 +0530

    Fix Dds switch issue for wifi and IWLAN scenarios
    In wifi on case, internet requests are released and there are
    no oustanding requests to process, so user triggered Dds switch will
    be blocked. Add a dummy network request to force Dds switch.
    
    If IWLAN is reported on non dds sub, move state machine to Attached
    state, to process any on demand network requests possible over IWLAN
    
    Change-Id: I07d3c693bb463afe6ad718caf1c511afe437f1ae
    CRs-Fixed: 993272

--- src/java/com/android/internal/telephony/dataconnection/DctController.java
+++ src/java/com/android/internal/telephony/dataconnection/DctController.java
@@ -553,6 +553,7 @@ public class DctController extends Handler {
     }
 
     protected int getTopPriorityRequestPhoneId() {
+        RequestInfo retRequestInfo = null;
         String topSubId = null;
         int priority = -1;
         int subId;
@@ -562,12 +563,18 @@ public class DctController extends Handler {
             if (requestInfo.priority > priority) {
                 priority = requestInfo.priority;
                 topSubId = requestInfo.request.networkCapabilities.getNetworkSpecifier();
+                retRequestInfo = requestInfo;
             }
         }
         if (TextUtils.isEmpty(topSubId)) {
             subId = mSubController.getDefaultDataSubId();
         } else {
             subId = Integer.parseInt(topSubId);
+            if (apnForNetworkRequest(retRequestInfo.request).equals(
+                    PhoneConstants.APN_TYPE_IMS) && mNeedsDdsSwitch.get()) {
+                logd("getTopPriorityRequestPhoneId: ims request, use dds phone id");
+                subId = mSubController.getDefaultDataSubId();
+            }
         }
         final int phoneId = mSubController.getPhoneId(subId);
         if (phoneId == DEFAULT_PHONE_INDEX) {
